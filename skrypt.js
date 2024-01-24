aa6eb068c63857d13d3615fd3d9290e8e187b6a8
Michał Kutzner <michal.kutzner@pl.abb.com>
2024-01-24 18:51:04 +0100

    Dodana lekcja 12

---
+++ skrypt.js
@@ -0,0 +1,8 @@
+function naKlik() {
+    alert("Kliknąłeś H1!");
+}
+
+var imie = prompt("Jak masz na imię?");
+console.log(imie)
+alert("Witaj " + imie + " !")
+document.write("<p>Cześć " + imie + "</p>")
